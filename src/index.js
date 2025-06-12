ae26926f06972f09be756b5c6c3121b68e72d316
Ackerman <anonymous310702@gmail.com>
2025-06-12 19:17:57 +0530

    Setting up basic project files

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./.env'
+})
+const port = process.env.PORT;
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error: ",error);
+        throw error;
+    })
+    app.listen(port || 8000 , ()=>{
+        console.log(`Server is running on Port ${port}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed!!!", err);
+})
+;
